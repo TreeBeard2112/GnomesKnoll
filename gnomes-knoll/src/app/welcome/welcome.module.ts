d873f20a70f6c28adb779c7c542cbe973e0e436f
TreeBeard2112 <jansensd@mail.uc.edu>
2022-02-03 15:09:35 -0500

    added store, gallery, info components, flex = WIP

--- gnomes-knoll/src/app/welcome/welcome.module.ts
+++ gnomes-knoll/src/app/welcome/welcome.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { WelcomeComponent } from './welcome.component';
 import { WelcomeCardComponent } from './components/welcome-card/welcome-card.component';
 import { FlexModule } from '@angular/flex-layout';
+import { GalleryCardComponent } from './components/gallery-card/gallery-card.component';
+import { InfoCardComponent } from './components/info-card/info-card.component';
+import { StoreCardComponent } from './components/store-card/store-card.component';
 
 
 
 @NgModule({
   declarations: [
     WelcomeComponent,
-    WelcomeCardComponent
+    WelcomeCardComponent,
+    GalleryCardComponent,
+    InfoCardComponent,
+    StoreCardComponent
   ],
   imports: [
     BrowserModule,
